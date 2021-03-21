c2b7c9c9569c20e4ce888d6f72a83e578ddd7393
Lakshika Medhavini <lakshikamedhavini@gmail.com>
2021-03-21 13:33:49 +0530

    interfaces

---
+++ app/src/main/java/com/example/myapplication/checkOutFragment.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link checkOutFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class checkOutFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_check_out, container, false);
+    }
+}
